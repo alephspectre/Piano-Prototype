107aa9fe3b76350caea5f805042fc4476aa39541
Julian Ceipek <julian.ceipek@gmail.com>
2011-11-12 01:06:15 -0500

    Added note class with constructor.

---
+++ PianoGame/PianoGame/PianoGame/Note.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace PianoGame
+{
+    class Note
+    {
+        public Vector2 position; //The position consists of a y position and a time
+        public float leftRange;
+        public float rightRange; //
+        public byte type; //The type of the note determines the image associated with it
+        public byte keyToPress; //The correct key associated with this note
+        public Note(Vector2 pos, float lR, float rR, byte tpe, byte kTP)
+        {
+            position = pos;
+            leftRange = lR;
+            rightRange = rR;
+            type = tpe;
+            keyToPress = kTP;
+        }
+    }
+}
