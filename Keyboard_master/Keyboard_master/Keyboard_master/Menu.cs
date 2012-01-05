348d0a42a9884ccc361e890558cafd705e9690b8
Nathan Lintz <nlintz@gmail.com>
2012-01-05 14:59:54 -0500

    made basic structure for the menu

--- Keyboard_master/Keyboard_master/Keyboard_master/Menu.cs
+++ Keyboard_master/Keyboard_master/Keyboard_master/Menu.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using System.Text;
+
 
 namespace Keyboard_master
 {
-    class Menu
+    class Menu : IDisposable
     {
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            
+        }
+
+        public void Update(GameTime gameTime)
+        {
+        
+        }
+
+        public ContentManager Content 
+        {
+            get { return content; }  
+        }
+        ContentManager content;
+
+        public void Dispose()
+        {
+            Content.Unload();
+        }
+
+
+
     }
 }
